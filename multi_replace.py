d6f1e500c90a21a41d3c65873d0b4c4b05c80f07
tets331 <ytetsuya331@gmail.com>
2018-12-29 10:04:16 +0900

    add new file

---
+++ multi_replace.py
@@ -0,0 +1,40 @@
+import re
+
+class multi_replace:
+    def __init__(self, *args, **kwds):
+        self.adict = dict(*args, **kwds)
+        self.rx = self.make_rx()
+    def make_rx(self):
+        return re.compile(r'|'.join(map(re.escape, self.adict)))
+    def one_dict(self, match):
+        return self.adict[match.group(0)]
+    def __call__(self, text):
+        return self.rx.sub(self.one_dict, text)
+
+class multi_replace_by_whole_words(multi_replace):
+    def make_rx(self):
+        return re.compile('|'.join(map(self.get_pattern, self.adict)))
+    def get_pattern(self, key):
+        pattern = re.escape(key)
+        if key[0].islower():
+            pattern = r'(?<![a-z])%s' % (pattern)
+        else:
+            pattern = r'(?<![A-Z])%s' % (pattern)
+        if key[-1].islower():
+            pattern = r'%s(?![a-z])' % (pattern)
+        else:
+            pattern = r'%s(?![A-Z])' % (pattern)
+        return pattern
+
+if __name__ == "__main__":
+    adict = {
+        'This':'That',
+        'this':'that',
+        'is':'was',
+        'am':'was',
+        'whole':'entire',
+        'word':'string'
+    }
+    s = 'This is sample code for this "multi_replace_by_whole_words".'
+    mrw = multi_replace_by_whole_words(adict)
+    print mrw(s)
